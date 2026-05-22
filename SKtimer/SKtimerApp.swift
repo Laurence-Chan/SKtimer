c8f5fe2996f32be35ac13b409f2f50917e6244c6
Laurence Chan <ds.chan.tfrg@gmail.com>
2026-05-23 07:53:05 +0800

    Initial Commit

---
+++ SKtimer/SKtimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKtimerApp.swift
+//  SKtimer
+//
+//  Created by Laurence Chan on 2026/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SKtimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SKtimerTests/SKtimerTests.swift
@@ -0,0 +1,19 @@
+//
+//  SKtimerTests.swift
+//  SKtimerTests
+//
+//  Created by Laurence Chan on 2026/5/23.
+//
+
+import Testing
+@testable import SKtimer
+
+struct SKtimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
